9bc86eafd1335afdbeeae2fa03b2c7a059bafd35
Ethan Pierce <ethanpiercepresents@gmail.com>
2022-01-13 14:37:49 -0800

    animation testing

--- animations/transition.js
+++ animations/transition.js
@@ -1,1 +1,5 @@
-import gsap from 'gsap';
+import { gsap } from 'gsap';
+const tl = gsap.timeline();
+export const hiddenText = (elem) => {
+  tl.to(elem, { x: '-948%', ease: 'power2.out', duration: 1 });
+};
